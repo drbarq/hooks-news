0fea71afa459b5c9e556a932fb08cfa7ebe712c1
drbarq <J.Tustin@gmail.com>
2019-11-12 16:13:43 -0700

    adding in header login logic

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 import Header from './Header'
+import useAuth from '../components/Auth/useAuth'
+// import FirebaseContext from '../firebase/context'
+import firebase, { FirebaseContext } from '../firebase'
 
 import Login from '../components/Auth/Login'
 import ForgotPassword from '../components/Auth/ForgotPassword'
@@ -11,24 +14,28 @@ import LinkList from '../components/Link/LinkList'
 import LinkDetail from '../components/Link/LinkDetail'
 
 function App() {
+  const user = useAuth()
+
   return (
     <BrowserRouter>
-      <div className="app-container">
-        <Header />
-        <div className="route-container">
-          <Switch>
-            <Route path="/" exact render={() => <Redirect to="/new/1" />} />
-            <Route path="/create" component={CreateLink} />
-            <Route path="/login" component={Login} />
-            <Route path="/forgot" component={ForgotPassword} />
-            <Route path="/search" component={SearchLinks} />
-            <Route path="/top" component={LinkList} />
-            <Route path="/new/:page" component={LinkList} />
-            <Route path="/link/:linkID" component={LinkDetail} />
-          </Switch>
+      <FirebaseContext.Provider value={{user, firebase}}>
+        <div className="app-container">
+          <Header />
+          <div className="route-container">
+            <Switch>
+              <Route path="/" exact render={() => <Redirect to="/new/1" />} />
+              <Route path="/create" component={CreateLink} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot" component={ForgotPassword} />
+              <Route path="/search" component={SearchLinks} />
+              <Route path="/top" component={LinkList} />
+              <Route path="/new/:page" component={LinkList} />
+              <Route path="/link/:linkID" component={LinkDetail} />
+            </Switch>
+          </div>
         </div>
-      </div>
-      </BrowserRouter>
+      </FirebaseContext.Provider>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { withRouter, NavLink } from 'react-router-dom'
+import { FirebaseContext } from "../firebase";
 
 function Header() {
+  const { user, firebase } = React.useContext(FirebaseContext)
+
   return (
     <div className="header">
       <div className="flex">
@@ -12,23 +15,33 @@ function Header() {
         <NavLink to="/" className="header-link">
           new
         </NavLink>
-        <div className="divider"></div>
+        <div className="divider">|</div>
         <NavLink to="/top" className="header-link">
           top
         </NavLink>
-        <div className="divider"></div>
+        <div className="divider">|</div>
         <NavLink to="/search" className="header-link">
           search
         </NavLink>
-        <div className="divider"></div>
+        <div className="divider">|</div>
         <NavLink to="/create" className="header-link">
           submit
         </NavLink>
-        <div className="flex">
-          <NavLink to="/login" className="header-link">
-            login
-          </NavLink>
         </div>
+        <div className="flex">
+          {
+            user 
+            ? (
+                <>
+                  <div className="header-name">{user.displayName}</div>
+                  <div className="divider">|</div>
+                  <div className="header-button">logout</div>
+                </>
+              ) 
+            : (<NavLink to="/login" className="header-link">
+                login
+              </NavLink>)
+          }
       </div>
     </div>
   )
