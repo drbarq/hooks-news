dcf5b5e3e0722f935971bb0f1532498c0b38695d
drbarq <J.Tustin@gmail.com>
2019-11-12 14:56:17 -0700

    adding in login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,24 +1,40 @@
 import React from "react";
+import useFormValidation from './useFormValidation'
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: ""
+}
 
 function Login(props) {
+  const { handleChange, handleSubmit, values } = useFormValidation(INITIAL_STATE)
   const [login, setLogin] =  React.useState(true)
-
 
   return (
     <div>
       <h2 className="mv3"> {login ? "Login" : "Create Account"}  </h2>
-      <form className="flex flex-column">
-        { !login && <input
+      <form onSubmit={handleSubmit} className="flex flex-column">
+        { !login && (<input
+          onChange={handleChange}
+          value={values.name}
+          name="name"
           type="text"
           placeholder="Your name"
           autoComplete="off"
-        />}
+        />)}
         <input
+          onChange={handleChange}
+          value={values.email}
+          name="email"
           type="email"
           placeholder="Your email"
           autoComplete="off"
         />
         <input
+          onChange={handleChange}
+          value={values.password}
+          name="password"
           type="password"
           placeholder="Choose a secure password"
           autoComplete="off"
