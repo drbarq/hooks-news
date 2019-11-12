14751a78bbd5dcb5ce835855eb7c927d70a8b952
drbarq <J.Tustin@gmail.com>
2019-11-12 14:11:39 -0700

    Adding in router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+
+import Login from '../components/Auth/Login'
+import ForgotPassword from '../components/Auth/ForgotPassword'
+import CreateLink from '../components/Link/CreateLink'
+import SearchLinks from '../components/Link/SearchLinks'
+import LinkList from '../components/Link/LinkList'
+import LinkDetail from '../components/Link/LinkDetail'
 
 function App() {
   return (
-    <div style={{ margin: "0 auto", textAlign: "center" }}>
-      <img style={{ height: "100vh" }} src="/logo.png" alt="Logo" />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact render={() => <Redirect to="/new/1" />} />
+        <Route path="/create" component={CreateLink} />
+        <Route path="/login" component={Login} />
+        <Route path="/forgot" component={ForgotPassword} />
+        <Route path="/search" component={SearchLinks} />
+        <Route path="/top" component={LinkList} />
+        <Route path="/new/:page" component={LinkList} />
+        <Route path="/link/:linkID" component={LinkDetail} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
