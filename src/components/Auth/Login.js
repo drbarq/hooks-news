586f93b2c7e856f7087a3ec51615559e77f52c0f
drbarq <J.Tustin@gmail.com>
2019-11-12 14:26:25 -0700

    Adding in login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 function Login(props) {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h2 className="mv3">Create Account</h2>
+      <form className="flex flex-column">
+        <input
+          type="text"
+          placeholder="Your name"
+          autoComplete="off"
+        />
+        <input
+          type="email"
+          placeholder="Your email"
+          autoComplete="off"
+        />
+        <input
+          type="password"
+          placeholder="Choose a secure password"
+          autoComplete="off"
+        />
+        <div className="flex mt3">
+          <button
+            type="submit"
+            className="button pointer mr2"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Login;
