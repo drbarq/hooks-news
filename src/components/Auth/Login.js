9c491a3ff3b17af7c69b807ade2f00eb0e06b9e8
drbarq <J.Tustin@gmail.com>
2019-11-12 14:34:20 -0700

    Adding in login logic

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 function Login(props) {
+  const [login, setLogin] =  React.useState(true)
+
+
   return (
     <div>
-      <h2 className="mv3">Create Account</h2>
+      <h2 className="mv3"> {login ? "Login" : "Create Account"}  </h2>
       <form className="flex flex-column">
-        <input
+        { !login && <input
           type="text"
           placeholder="Your name"
           autoComplete="off"
-        />
+        />}
         <input
           type="email"
           placeholder="Your email"
@@ -26,6 +29,14 @@ function Login(props) {
             className="button pointer mr2"
           >
             Submit
+          </button>
+          <button
+            type="button"
+            className="pointer button"
+            // onClick={() => setLogin(!login)}
+            onClick={() => setLogin(prevLogin => !prevLogin)}
+          >
+            { login ? "need to create an account?" : "already have an account?"}
           </button>
         </div>
       </form>
