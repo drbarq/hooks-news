ed1b19d9e3aff9c23400b1ebf9ea23e57158321a
drbarq <J.Tustin@gmail.com>
2019-11-12 14:22:13 -0700

    Addin in header

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
+import Header from './Header'
+
 import Login from '../components/Auth/Login'
 import ForgotPassword from '../components/Auth/ForgotPassword'
 import CreateLink from '../components/Link/CreateLink'
@@ -11,17 +13,22 @@ import LinkDetail from '../components/Link/LinkDetail'
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact render={() => <Redirect to="/new/1" />} />
-        <Route path="/create" component={CreateLink} />
-        <Route path="/login" component={Login} />
-        <Route path="/forgot" component={ForgotPassword} />
-        <Route path="/search" component={SearchLinks} />
-        <Route path="/top" component={LinkList} />
-        <Route path="/new/:page" component={LinkList} />
-        <Route path="/link/:linkID" component={LinkDetail} />
-      </Switch>
-    </BrowserRouter>
+      <div className="app-container">
+        <Header />
+        <div className="route-container">
+          <Switch>
+            <Route path="/" exact render={() => <Redirect to="/new/1" />} />
+            <Route path="/create" component={CreateLink} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot" component={ForgotPassword} />
+            <Route path="/search" component={SearchLinks} />
+            <Route path="/top" component={LinkList} />
+            <Route path="/new/:page" component={LinkList} />
+            <Route path="/link/:linkID" component={LinkDetail} />
+          </Switch>
+        </div>
+      </div>
+      </BrowserRouter>
   );
 }
 
