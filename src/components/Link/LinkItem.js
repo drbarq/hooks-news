c9725f3e80d953c3cd73e230484e657e462f5a5d
drbarq <J.Tustin@gmail.com>
2019-11-13 15:49:21 -0700

    adding in upvoting

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -1,14 +1,40 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import { getDomain } from '../../utils'
 import distanceInWordsToNow from 'date-fns/distance_in_words_to_now'
+import { FirebaseContext } from "../../firebase";
 
-function LinkItem({link, index, showCount}) {
+function LinkItem({link, index, showCount, history}) {
+
+  const { firebase, user } = React.useContext(FirebaseContext)
+  
+  function handleVote() {
+    if (!user) {
+      history.push('/login')
+    } else {
+      const voteRef = firebase.db.collection('links').doc(link.id)   
+      voteRef.get().then(doc => {
+        if (doc.exists) {
+          const previousVotes = doc.data().votes
+          const vote = { votedBy: { id: user.uid, name: user.displayName} }
+          const updatedVotes = [...previousVotes, vote]
+          voteRef.update({ votes: updatedVotes})
+        }
+      })
+      // to update a document in fb you need the reference, and the full document
+      // possible it wont be able to find it, need to have a back up case .exists
+      // specify what value specifically you want to update
+    }
+  }
+
+
+
+
   return (
     <div className="flex items-start mt2">
       <div className="flex items-center">
         {showCount && <span className="gray">{index}</span>}
-        <div className="vote-button">↑</div>
+        <div className="vote-button" onClick={handleVote}>↑</div>
       </div>
       <div className="ml1">
         <div>
@@ -28,4 +54,4 @@ function LinkItem({link, index, showCount}) {
   )
 }
 
-export default LinkItem;
+export default withRouter(LinkItem);
