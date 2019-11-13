21d2dce44f841377fa5adb40bb3ee19aa26b926a
drbarq <J.Tustin@gmail.com>
2019-11-13 14:22:31 -0700

    adding in password reset

--- src/components/Auth/ForgotPassword.js
+++ src/components/Auth/ForgotPassword.js
@@ -1,7 +1,41 @@
 import React from "react";
+import FirebaseContext from '../../firebase/context'
 
 function ForgotPassword() {
-  return <div>ForgotPassword</div>;
+  const { firebase } = React.useContext(FirebaseContext)
+  const [ resetPasswordEmail, setResetPasswordEmail] = React.useState('')
+  const [isPasswordReset, setIsPasswordReset] = React.useState(false)
+  const [passwordResetError, setPasswordError] = React.useState(null)
+
+  async function handleResetPassword() {
+    try {
+      await firebase.resetPassword(resetPasswordEmail)
+      setIsPasswordReset(true)
+    } catch (err) {
+      console.error("Error Sending Email", err)
+      setPasswordError(err.message)
+      setIsPasswordReset(false)
+    }
+  }
+
+  return (
+    <div>
+      <input 
+        type="email"
+        className="input"
+        placeholder="Provide your account email"
+        onChange={event => setResetPasswordEmail(event.target.value)}
+      />
+      <div>
+        <button className="button" onClick={handleResetPassword}>
+          Reset Password
+        </button>
+      </div>
+      {isPasswordReset && <p>Check email to reset password</p>}
+      {passwordResetError && <p className="error-text">{ passwordResetError }</p>}
+    </div>
+
+  )
 }
 
 export default ForgotPassword;
