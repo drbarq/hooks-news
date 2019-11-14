7181fa38b75d963006edd525e84c062ced0a646e
drbarq <J.Tustin@gmail.com>
2019-11-14 11:04:20 -0700

    Addinging in pagenation

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -17,7 +17,8 @@ function LinkItem({link, index, showCount, history}) {
           const previousVotes = doc.data().votes
           const vote = { votedBy: { id: user.uid, name: user.displayName} }
           const updatedVotes = [...previousVotes, vote]
-          voteRef.update({ votes: updatedVotes})
+          const voteCount = updatedVotes.length
+          voteRef.update({ votes: updatedVotes, voteCount: voteCount})
         }
       })
       // to update a document in fb you need the reference, and the full document
@@ -37,7 +38,6 @@ function LinkItem({link, index, showCount, history}) {
     })
   }
 
-
   return (
     <div className="flex items-start mt2">
       <div className="flex items-center">
@@ -49,7 +49,7 @@ function LinkItem({link, index, showCount, history}) {
          <a href={link.url} className="black no-underline">{link.description}</a> <span className="link">({getDomain(link.url)})</span>
         </div> 
         <div className="f6 lh-copy grey">
-          {link.votes.length} votes by {link.postedBy.name} {distanceInWordsToNow(link.created)}
+          {link.voteCount} votes by {link.postedBy.name} {distanceInWordsToNow(link.created)}
           {" | "}
           <Link to={`/link/${link.id}`}>
             {link.comments.length > 0 
@@ -69,3 +69,76 @@ function LinkItem({link, index, showCount, history}) {
 }
 
 export default withRouter(LinkItem);
+
+
+// PRE PAGNATION 
+// import React from "react";
+// import { Link, withRouter } from 'react-router-dom'
+// import { getDomain } from '../../utils'
+// import distanceInWordsToNow from 'date-fns/distance_in_words_to_now'
+// import { FirebaseContext } from "../../firebase";
+
+// function LinkItem({link, index, showCount, history}) {
+//   const { firebase, user } = React.useContext(FirebaseContext)
+  
+//   function handleVote() {
+//     if (!user) {
+//       history.push('/login')
+//     } else {
+//       const voteRef = firebase.db.collection('links').doc(link.id)   
+//       voteRef.get().then(doc => {
+//         if (doc.exists) {
+//           const previousVotes = doc.data().votes
+//           const vote = { votedBy: { id: user.uid, name: user.displayName} }
+//           const updatedVotes = [...previousVotes, vote]
+//           voteRef.update({ votes: updatedVotes})
+//         }
+//       })
+//       // to update a document in fb you need the reference, and the full document
+//       // possible it wont be able to find it, need to have a back up case .exists
+//       // specify what value specifically you want to update
+//     }
+//   }
+//   // was the link posted by the same user who wants to delete it?
+//   const postedByAuthUser = user && user.uid === link.postedBy.id
+
+//   function handleDeleteLink() {
+//     const linkRef = firebase.db.collection('links').doc(link.id)
+//     linkRef.delete().then(() => {
+//       console.log(`Document with ID ${link.id} deleted`)
+//     }).catch(err => {
+//       console.error(`Error deleting document:`, err)
+//     })
+//   }
+
+//   return (
+//     <div className="flex items-start mt2">
+//       <div className="flex items-center">
+//         {showCount && <span className="gray">{index}</span>}
+//         <div className="vote-button" onClick={handleVote}>↑</div>
+//       </div>
+//       <div className="ml1">
+//         <div>
+//          <a href={link.url} className="black no-underline">{link.description}</a> <span className="link">({getDomain(link.url)})</span>
+//         </div> 
+//         <div className="f6 lh-copy grey">
+//           {link.votes.length} votes by {link.postedBy.name} {distanceInWordsToNow(link.created)}
+//           {" | "}
+//           <Link to={`/link/${link.id}`}>
+//             {link.comments.length > 0 
+//             ? `${link.comments.length} comments`
+//             : "discuss"}
+//           </Link>
+//           {postedByAuthUser && (
+//             <>
+//             {" | "}
+//             <span className="delete-button" onClick={handleDeleteLink}>delete</span>
+//             </>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default withRouter(LinkItem);
--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -6,14 +6,21 @@ function LinkList(props) {
   const { firebase } = React.useContext(FirebaseContext)
   const [ links, setLinks ] = React.useState([])
   const isNewPage = props.location.pathname.includes("new")
+  const isTopPage = props.location.pathname.includes("top")
+
 
   React.useEffect(() => {
-    getLinks()
-  }, [])
+    const unsubscribe = getLinks()
+    return () => unsubscribe()
+  }, [isTopPage])
 
   function getLinks() {
     // firebase.db.collection('links').onSnapshot(handleSnapshot)
-    firebase.db.collection('links').orderBy('created', 'desc').onSnapshot(handleSnapshot)
+    if (isTopPage) {
+      return firebase.db.collection('links').orderBy('voteCount', 'desc').onSnapshot(handleSnapshot)
+    } else  {
+      return firebase.db.collection('links').orderBy('created', 'desc').onSnapshot(handleSnapshot)
+    }
     // get all the links collection and sort them 
   }
 
@@ -25,19 +32,19 @@ function LinkList(props) {
     console.log(links)
   }
 
-  function renderLinks() {
-    // return the newest links or the top links depending on the page
-    if (isNewPage) {
-      return links
-    }
-    const topLinks = links.slice().sort((link1, link2) => link2.votes.length - link1.votes.length)
-    return topLinks
-  }
+  // function renderLinks() {
+  //   // return the newest links or the top links depending on the page
+  //   if (isNewPage) {
+  //     return links
+  //   }
+  //   const topLinks = links.slice().sort((link1, link2) => link2.votes.length - link1.votes.length)
+  //   return topLinks
+  // }
 
 
   return (
     <div>
-      {renderLinks().map((link, index) => (
+      {links.map((link, index) => (
         <LinkItem key={link.id} showCount={true} link={link} index={index + 1}  />
       ))}
     </div>
@@ -46,15 +53,51 @@ function LinkList(props) {
 
 export default LinkList;
 
-
-
+// Pre Pagation
 // import React from "react";
-// import useAuth from '../Auth/useAuth'
+// import { FirebaseContext } from "../../firebase";
+// import LinkItem from './LinkItem'
 
 // function LinkList(props) {
-//   const user = useAuth()
-//   console.log(user)
-//   return <div>LinkList</div>;
+//   const { firebase } = React.useContext(FirebaseContext)
+//   const [ links, setLinks ] = React.useState([])
+//   const isNewPage = props.location.pathname.includes("new")
+
+//   React.useEffect(() => {
+//     getLinks()
+//   }, [])
+
+//   function getLinks() {
+//     // firebase.db.collection('links').onSnapshot(handleSnapshot)
+//     firebase.db.collection('links').orderBy('created', 'desc').onSnapshot(handleSnapshot)
+//     // get all the links collection and sort them 
+//   }
+
+//   function handleSnapshot(snapshot) {
+//     const links = snapshot.docs.map(doc => {
+//       return {id: doc.id, ...doc.data() }
+//     })
+//     setLinks(links)
+//     console.log(links)
+//   }
+
+//   function renderLinks() {
+//     // return the newest links or the top links depending on the page
+//     if (isNewPage) {
+//       return links
+//     }
+//     const topLinks = links.slice().sort((link1, link2) => link2.votes.length - link1.votes.length)
+//     return topLinks
+//   }
+
+
+//   return (
+//     <div>
+//       {renderLinks().map((link, index) => (
+//         <LinkItem key={link.id} showCount={true} link={link} index={index + 1}  />
+//       ))}
+//     </div>
+//   )
 // }
 
 // export default LinkList;
