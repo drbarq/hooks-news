d2c3f38ff1af4c01524a6732bae4e94e4604345a
drbarq <J.Tustin@gmail.com>
2019-11-14 11:24:02 -0700

    pageination complete

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,25 +1,31 @@
 import React from "react";
 import { FirebaseContext } from "../../firebase";
 import LinkItem from './LinkItem'
+import { LINKS_PER_PAGE } from '../../utils/index'
 
 function LinkList(props) {
   const { firebase } = React.useContext(FirebaseContext)
   const [ links, setLinks ] = React.useState([])
+  const [cursor, setCursor] = React.useState(null)
   const isNewPage = props.location.pathname.includes("new")
   const isTopPage = props.location.pathname.includes("top")
+  const page = Number(props.match.params.page)
 
 
   React.useEffect(() => {
     const unsubscribe = getLinks()
     return () => unsubscribe()
-  }, [isTopPage])
+  }, [isTopPage, page])
 
   function getLinks() {
+    const hasCursor = Boolean(cursor)
     // firebase.db.collection('links').onSnapshot(handleSnapshot)
     if (isTopPage) {
-      return firebase.db.collection('links').orderBy('voteCount', 'desc').onSnapshot(handleSnapshot)
-    } else  {
-      return firebase.db.collection('links').orderBy('created', 'desc').onSnapshot(handleSnapshot)
+      return firebase.db.collection('links').orderBy('voteCount', 'desc').limit(LINKS_PER_PAGE).onSnapshot(handleSnapshot)
+    } else if (page === 1)  {
+      return firebase.db.collection('links').orderBy('created', 'desc').limit(LINKS_PER_PAGE).onSnapshot(handleSnapshot)
+    } else if (hasCursor) {
+      return firebase.db.collection('links').orderBy('created', 'desc').startAfter(cursor.created).limit(LINKS_PER_PAGE).onSnapshot(handleSnapshot)
     }
     // get all the links collection and sort them 
   }
@@ -28,25 +34,35 @@ function LinkList(props) {
     const links = snapshot.docs.map(doc => {
       return {id: doc.id, ...doc.data() }
     })
+    const lastLink = links[links.length - 1]
     setLinks(links)
+    setCursor(lastLink)
     console.log(links)
   }
 
-  // function renderLinks() {
-  //   // return the newest links or the top links depending on the page
-  //   if (isNewPage) {
-  //     return links
-  //   }
-  //   const topLinks = links.slice().sort((link1, link2) => link2.votes.length - link1.votes.length)
-  //   return topLinks
-  // }
+  function visitPreviousPage() {
+    props.history.push(`/new/${page -1}`)
+  }
 
+  function visitNextPage() {
+    if (page <= links.length / LINKS_PER_PAGE) {
+      props.history.push(`/new/${page + 1}`)
+    }
+  }
+
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 1 
 
   return (
     <div>
       {links.map((link, index) => (
-        <LinkItem key={link.id} showCount={true} link={link} index={index + 1}  />
+        <LinkItem key={link.id} showCount={true} link={link} index={index + pageIndex}  />
       ))}
+      {isNewPage && (
+        <div className="pagination">
+          <div className="pointer mr2" onClick={visitPreviousPage}>Previous</div>
+          <div className="pointer" onClick={visitNextPage}>Next</div>
+        </div>
+      )}
     </div>
   )
 }
